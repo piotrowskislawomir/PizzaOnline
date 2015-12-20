3ecf0303428cf7bbf9651a7e48dbc92a97b77fd7
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2015-12-20 18:43:13 +0100

    added adding ingredient with unit tests in red step

--- PizzaOnline.Tests/Services/IngredientServiceTests.cs
+++ PizzaOnline.Tests/Services/IngredientServiceTests.cs
@@ -1,6 +1,34 @@
-﻿namespace PizzaOnline.Tests.Services
+﻿using FakeItEasy;
+using NUnit.Framework;
+using PizzaOnline.Model;
+using PizzaOnline.Services;
+using PizzaOnline.Storage;
+
+namespace PizzaOnline.Tests.Services
 {
+    [TestFixture]
     public class IngredientServiceTests
     {
+        private IngredientService _sut;
+        private IRepository<Ingredient> _ingredientsRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _ingredientsRepository = A.Fake<IRepository<Ingredient>>();
+            _sut = new IngredientService(_ingredientsRepository);
+        }
+
+        [Test]
+        public void Add_Should_Persist_Ingredient_In_Repository()
+        {
+            var ingredient = new Ingredient();
+
+            _sut.Add(ingredient);
+            
+            A.CallTo(() => _ingredientsRepository.Persist(A<Ingredient>._))
+                .MustHaveHappened();
+        }
+
     }
 }
--- PizzaOnline/Services/IngredientService.cs
+++ PizzaOnline/Services/IngredientService.cs
@@ -1,6 +1,19 @@
-﻿namespace PizzaOnline.Services
+﻿using PizzaOnline.Model;
+using PizzaOnline.Storage;
+
+namespace PizzaOnline.Services
 {
     public class IngredientService :IIngredientService
     {
+        private readonly IRepository<Ingredient> _ingredinetRepository;
+        public IngredientService(IRepository<Ingredient> ingredinetRepository)
+        {
+            _ingredinetRepository = ingredinetRepository;
+        }
+
+        public Ingredient Add(Ingredient ingredient)
+        {
+            return null;
+        }
     }
 }
