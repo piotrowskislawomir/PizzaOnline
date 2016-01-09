e0ac8620f5de4a57015afafe2d3c16dd874c804b
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2016-01-09 09:45:50 +0100

    added view for ingredient page

---
+++ PizzaOnline.Web/Controllers/IngredientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaOnline.Web.Controllers
+{
+    public class IngredientController : Controller
+    {
+        // GET: Ingredient
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
