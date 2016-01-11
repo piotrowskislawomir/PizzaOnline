9c916059cc189fe835ccdee63c09a6e459cb9682
MichalMasztalerz <ttrolu@gmail.com>
2016-01-11 01:21:08 +0100

    Ingredients Web

--- PizzaOnline.Web/scripts/Ingredients.js
+++ PizzaOnline.Web/scripts/Ingredients.js
@@ -1,32 +1,61 @@
 ﻿$(document).ready(function() {
 
-$("#add_ingredient").click(
-    function(event) {
+    $("#add_ingredient").click(
+    function (event) {
     var ingredient = {
-            name: $("#ingredient_name").val(),
-            price: parseFloat($("#ingredient_price").val())
-        };
+        name: $("#ingredient_name").val(),
+        price: parseFloat($("#ingredient_price").val())
+    };
 
-        var ingedientJson = JSON.stringify(ingredient);
-        $.ajax({
-            dataType: "json",
-            contentType: "application/json",
-            url: 'http://localhost:5413/api/ingredient',
-            type: 'POST',
-            data: ingedientJson,
-            success: function() {
-              //  alert('POST OK'); 
-            }
-        });
-        event.preventDefault();
-        $('#ingredient_name').val("");
-        $('#ingredient_price').val("");
-        getAllIngredients();
+    var ingedientJson = JSON.stringify(ingredient);
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json",
+        url: 'http://localhost:5413/api/ingredient',
+        type: 'POST',
+        data: ingedientJson,
+        success: function () {
+          
+            getAllIngredients();
+        }
     });
+      $('#ingredient_name').val("");
+      $('#ingredient_price').val("");
+      event.preventDefault();
+    });
+    
+    getAllIngredients();
 });
 
+function removeIngredient(obj) {
+
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json",
+        url: 'http://localhost:5413/api/ingredient/' + obj.id,
+        type: 'DELETE',
+        success: function () {
+            alert("USUWANIE");
+        }
+
+    });
+
+    getAllIngredients();
+    $.blockUI({ message: '<h1>Trwa usuwanie produktu...</h1>' });
+    setTimeout($.unblockUI, 4000);
+    location.reload();
+}
+
+function clearTable() {
+  //  $("#ingredients_table tr").empty();
+    $("#ingredients_table tr:gt(2)").detach();
+}
+
 function getAllIngredients() {
-    {
+    
+   if ($('#ingredients_table tr').length > 2)
+       clearTable();
+   
         $.ajax({
             dataType: "json",
             contentType: "application/json",
@@ -34,9 +63,12 @@ function getAllIngredients() {
             type: 'GET',
             success: function (data) {
                 for (var i = 0; i < data.length; i++) {
-                    $(".ingredients_table").append("<tr><td>" + data[i].name + "</td><td>" + data[i].price + "</td></tr>");
+                    $("#ingredients_table").append("<tr><th>" + data[i].name + "</th><th>" + data[i].price + "</th>" +
+                        "<th><button onClick=\"removeIngredient(this);\" class=\"ingredient_remove_btn\" id=" + data[i].id + ">Usuń składnik</button></th></tr>");
                 }
             }
         });
-    }
+   
 }
+
+
