5b2deca68228bf3af9d0006f1caaa0555e563fda
MichalMasztalerz <ttrolu@gmail.com>
2016-01-16 16:33:51 +0100

    Integration Tests GetAllWithToppings

--- PizzaOnline.Tests.Integration/Storage/PizzaRepositoryTests.cs
+++ PizzaOnline.Tests.Integration/Storage/PizzaRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using NUnit.Framework;
 using PizzaOnline.Model;
 using PizzaOnline.Storage;
@@ -15,6 +16,7 @@ namespace PizzaOnline.Tests.Integration.Storage
         {
             _sut = new PizzaRepository(_dbContextFactory);
             _ingreditentsRepository = new Repository<Ingredient>(_dbContextFactory);
+            _pizzasRepository = new Repository<Pizza>(_dbContextFactory);
         }
 
         private static Func<DbContext> _dbContextFactory;
@@ -30,6 +32,7 @@ namespace PizzaOnline.Tests.Integration.Storage
 
         private PizzaRepository _sut;
         private Repository<Ingredient> _ingreditentsRepository;
+        private Repository<Pizza> _pizzasRepository;
 
         [Test]
         public void GetByIdWithToppings_ShouldReturnPizzaWithIngredients_WhenIngredientsWereAdded()
@@ -57,6 +60,53 @@ namespace PizzaOnline.Tests.Integration.Storage
             Assert.That(result, Is.Not.Null);
             CollectionAssert.IsNotEmpty(result.PizzasIngredients);
             Assert.That(result.PizzasIngredients.Count, Is.EqualTo(expectedPizza.PizzasIngredients.Count));
+        }
+
+        [Test]
+        public void GetAllWithToppings_ShouldReturnAllPizzasWithIngredients_WhenIngredientsWereAdded()
+        {
+            var ingredient1 = new Ingredient { Name = "ingredient1", Price = 10 };
+            var ingredient2 = new Ingredient { Name = "ingredient2", Price = 20 };
+
+            _ingreditentsRepository.Persist(ingredient1);
+            _ingreditentsRepository.Persist(ingredient2);
+
+            var Pizza1 = new Pizza
+            {
+                Name = "pizza1",
+                Price = 123.12M,
+                PizzasIngredients = new List<PizzasIngredients>()
+                {
+                    new PizzasIngredients {IngredientId = ingredient1.Id.Value},
+                    new PizzasIngredients {IngredientId = ingredient2.Id.Value}
+                }
+            };
+
+            var Pizza2 = new Pizza
+            {
+                Name = "pizza1",
+                Price = 123.12M,
+                PizzasIngredients = new List<PizzasIngredients>()
+                {
+                    new PizzasIngredients {IngredientId = ingredient1.Id.Value},
+                    new PizzasIngredients {IngredientId = ingredient2.Id.Value}
+                }
+            };
+
+            _pizzasRepository.Persist(Pizza1);
+            _pizzasRepository.Persist(Pizza2);
+
+
+
+            var expectedPizzas = new List<Pizza>();
+            expectedPizzas.Add(Pizza1);
+            expectedPizzas.Add(Pizza2);
+
+            var result = _sut.GetAllWithToppings();
+
+            Assert.That(result, Is.Not.Null);
+            CollectionAssert.IsNotEmpty(result);
+            Assert.That(result.Count(), Is.EqualTo(expectedPizzas.Count));
         }
 
 
