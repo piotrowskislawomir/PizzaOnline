be9a1136b1f76d022e64de35a2fda765ba78d56f
MichalMasztalerz <ttrolu@gmail.com>
2016-01-16 21:55:36 +0100

    Added orders list

---
+++ PizzaOnline.Web/scripts/OrdersList.js
@@ -0,0 +1,41 @@
+﻿
+function getAllOrders() {
+
+    var Order1 =
+    {
+        
+    };
+
+    var Order2 =
+    {
+
+    };
+
+    var Order3 =
+    {
+
+    };
+
+
+
+
+    for (var i = 0; i < data.length; i++) {
+        $("#ingredients_table").append("<tr><th>" + data[i].name + "</th><th>" + data[i].price.toFixed(2) + "</th>" +
+            "<th><button onClick=\"removeIngredient(this);\" class=\"ingredient_remove_btn\" id=" + data[i].id + ">Usuń składnik</button></th></tr>");
+    }
+   
+    /*
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json",
+        url: 'http://localhost:5413/api/ingredient',
+        type: 'GET',
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                $("#ingredients_table").append("<tr><th>" + data[i].name + "</th><th>" + data[i].price.toFixed(2) + "</th>" +
+                    "<th><button onClick=\"removeIngredient(this);\" class=\"ingredient_remove_btn\" id=" + data[i].id + ">Usuń składnik</button></th></tr>");
+            }
+        }
+    });*/
+
+}
