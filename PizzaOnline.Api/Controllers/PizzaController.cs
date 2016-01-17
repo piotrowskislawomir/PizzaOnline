c023a3d25c966e7aabb673c28a7b982638e93ae4
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2016-01-17 17:20:29 +0100

    pizza controller get pizza green step

--- PizzaOnline.Api/Controllers/PizzaController.cs
+++ PizzaOnline.Api/Controllers/PizzaController.cs
@@ -34,7 +34,13 @@ namespace PizzaOnline.Api.Controllers
         public IHttpActionResult GetPizza(int id)
         {
             var pizza = _pizzaService.Get(id);
-            return Ok(Mapper.Map<PizzaModel>(pizza));
+
+            if (pizza != null)
+            {
+                return Ok(Mapper.Map<PizzaModel>(pizza));
+            }
+
+            return NotFound();
         }
 
         [Route("api/pizza")]
