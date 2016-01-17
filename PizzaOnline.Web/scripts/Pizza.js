de59bd2abdb3c25c3d33eeedfa1a0d6018be6879
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2016-01-17 17:10:15 +0100

    moved removing pizza to index list

--- PizzaOnline.Web/scripts/Pizza.js
+++ PizzaOnline.Web/scripts/Pizza.js
@@ -1,15 +1,36 @@
-﻿$(document).ready(function () {
-   
-    getAllPizzas();
+﻿var allPizzasItems = [];
+var controlOrderPizzaItems = [];
+var orderPrice = 0;
+
+$(document).ready(function () {
+
+    getAllPizzasToRemove();
+
 });
 
+function removePizza(obj) {
 
-function clearTable() {
-    $("#ingredients_table tr:gt(2)").detach();
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json",
+        url: 'http://localhost:5413/api/pizza/' + obj.id,
+        type: 'DELETE',
+        success: function () {
+            alert("USUWANIE");
+        }
+
+    });
+
+    getAllPizzasToRemove();
+    $.blockUI({ message: '<h1>Trwa usuwanie pizzy...</h1>' });
+    setTimeout($.unblockUI, 4000);
+    location.reload();
 }
 
-function getAllPizzas() {
-    clearTable();
+
+
+
+function getAllPizzasToRemove() {
 
     $.ajax({
         dataType: "json",
@@ -19,15 +40,14 @@ function getAllPizzas() {
         success: function (data) {
             for (var i = 0; i < data.length; i++) {
                 var ingredients = "";
-                for (var j = 0; j < data[i].toppings.length; j++) { ingredients += " " + data[i].toppings[j].name; }
+                for (var j = 0; j < data[i].toppings.length; j++) {
+                    ingredients += " " + data[i].toppings[j].name;
+                }
 
-                $("#pizzas").append("<tr><th title=\"Składniki wybranej pizzy: " + ingredients + "\"> " + data[i].name + "</br><font size=\"1\">" + ingredients + "</font></th><th>" + data[i].price.toFixed(2) + "</th>" +
-         "<th> <button type=\"button\" id=\"" + data[i].id + "\" onclick=\"deletePizza(this)\">Usuń</th></tr>");
+                $("#all_pizzas_admin_table").append("<tr><th title=\"Składniki wybranej pizzy: " + ingredients + "\"> " + data[i].name + "</br><font size=\"1\">" + ingredients + "</font></th><th>" + data[i].price.toFixed(2) + "</th>" +
+                  "<th> <button type=\"button\" id=\"" + data[i].id + "\" onclick=\"removePizza(this)\">Usuń pizze</th></tr>");
 
             }
         }
     });
-
-
-
 }
